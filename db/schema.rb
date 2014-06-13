8c93791e1b98ff9b152d5f5dba9e5c667fedde7e
Daniel Robinson <dan@tag.ca>
2014-06-13 17:27:14 -0400

    added passenger

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140606143009) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "infants", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
